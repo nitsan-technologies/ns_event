46d61b424d96ee2b1027c68abf8836df71310cf8
Rohan Parmar <rohan.nitsan@gmail.com>
2024-04-08 17:34:35 +0530

    [TASK] Update Extension Name

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF['ns_event'] = [
-    'title' => 'TYPO3 Event Manager',
+    'title' => 'Event Manager',
     'description' => 'TYPO3 Event Manager is a full-featured events management extension that lets you create multiple events and get them listed on your website. TYPO3  Event Manager provides a user-friendly interface that allows you to effortlessly create and customize events. Specify event details such as date, time, location, ticket types, and more in a matter of minutes.
     *** Live Demo: https://demo.t3planet.com/t3-extensions/pro/typo3-event-manager *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-event-extension',
     'category' => 'plugin',

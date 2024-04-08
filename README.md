17a260479dec48ababdba17cc6551ec66bbc7f92
Rohan Parmar <rohan.nitsan@gmail.com>
2024-04-08 17:32:06 +0530

    [TASK] Update Extension Details

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ns_event
-- Live Frontend Demo: 
-- Live Backend Demo: 
-- Free version: 
-- Premium version: 
-- Documentation: 
-- Changelog: 
+- Live Frontend Demo: https://demo.t3planet.com/t3-extensions/pro/typo3-event-manager
+- Live Backend Demo: https://demo.t3terminal.com/live-typo3/t3t-extensions/typo3/?TYPO3_AUTOLOGIN_USER=editor-ns-event
+- Free version: https://extensions.typo3.org/extension/ns_event
+- Premium version: https://t3planet.com/typo3-event-extension
+- Documentation: https://docs.t3planet.com/en/latest/ExtNsEvent/Index.html
+- Changelog: https://t3planet.com/typo3-event-extension
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,15 @@
 
 $EM_CONF['ns_event'] = [
     'title' => 'TYPO3 Event Manager',
-    'description' => 'TYPO3 Event Manager is a full-featured events management extension that lets you create multiple events and get them listed on your website. TYPO3  Event manager provides a user-friendly interface that allows you to effortlessly create and customize events. Specify event details such as date, time, location, ticket types, and more in a matter of minutes.',
+    'description' => 'TYPO3 Event Manager is a full-featured events management extension that lets you create multiple events and get them listed on your website. TYPO3  Event Manager provides a user-friendly interface that allows you to effortlessly create and customize events. Specify event details such as date, time, location, ticket types, and more in a matter of minutes.
+    *** Live Demo: https://demo.t3planet.com/t3-extensions/pro/typo3-event-manager *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-event-extension',
     'category' => 'plugin',
     'author' => 'T3D: Nilesh Malankiya, Rohan Parmar, Khushi Rupera, QA: Krishna Dhapa',
     'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'NITSAN Technologies Pvt Ltd',
+    'author_company' => 'T3Planet // NITSAN',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '11.0.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-12.5.99',

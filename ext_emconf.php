a1dd0d9381df36bc279ecb286c034907684b8543
Rohan Parmar <rohan.nitsan@gmail.com>
2024-04-10 10:45:59 +0530

    [TASK] Update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF['ns_event'] = [
     'description' => 'TYPO3 Event Manager is a full-featured events management extension that lets you create multiple events and get them listed on your website. TYPO3  Event Manager provides a user-friendly interface that allows you to effortlessly create and customize events. Specify event details such as date, time, location, ticket types, and more in a matter of minutes.
     *** Live Demo: https://demo.t3planet.com/t3-extensions/pro/typo3-event-manager *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-event-extension',
     'category' => 'plugin',
-    'author' => 'T3D: Nilesh Malankiya, Rohan Parmar, Khushi Rupera, QA: Krishna Dhapa',
+    'author' => 'T3D: Rohan Parmar, T3D:Khushi Rupera, T3D:Rushabh Makwana, QA:Krishna Dhapa',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'T3Planet // NITSAN',
     'state' => 'stable',
